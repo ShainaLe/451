3d0c5f5949aef26a6c3774968c9893901b0caaa2
ShainaLe <shaina.le@snhu.edu>
2023-08-06 11:10:44 -0500

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,4 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon 
 function myMenu() {
     var x = document.getElementById("myLinks");
     if (x.style.display === "block") {
@@ -6,4 +6,20 @@ function myMenu() {
     } else {
       x.style.display = "block";
     }
-  }
+  }*/
+
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".nav-menu");
+  
+  hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+  });
+  
+  document.querySelectorAll(".nav-link").forEach((link) =>
+    link.addEventListener("click", () => {
+      hamburger.classList.remove("active");
+      navMenu.classList.remove("active");
+    })
+  );
+  
